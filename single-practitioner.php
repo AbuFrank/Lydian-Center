ef0334ff6bc6fd17a665075c8e0902fd26aeaf74
juliesof <juliesof@gmail.com>
2018-03-22 13:18:22 -0400

    Add single practitioner file

---
+++ single-practitioner.php
@@ -0,0 +1,134 @@
+<?php
+/**
+*	 Template Name: Test
+* 
+* @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package lydiancenter
+ */
+
+	get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+			<?php 
+			while ( have_posts() ) : the_post(); ?>
+
+			<!-- Create variables for practitioners -->
+			<?php 
+				$first_name			= get_field('first_name');
+				$last_name      = get_field('last_name'); 								
+				$practice       = get_field('practice'); 
+			 	$certifications = get_field('certifications'); 
+			 	$about					= get_field('about');
+			 	$phone 					= get_field('phone_number');
+			 	$email					= get_field('email');
+			 	$p_photo 				= get_field('practitioner_photo');
+			 	$links 					= get_field('external_links');
+			?>
+
+			<!-- Retrieve testimonials object from database -->
+			<?php 
+					$testimonials = get_posts(array(
+						'post_type'				=> 'testimonials',
+						'posts_per_page'	=> -1
+					));
+				?>
+				<!-- Create accessible array of testimonials -->
+				<?php 
+					$posts = $testimonials;
+					$testimonials_array = array();
+
+					foreach( $posts as $post ): 			
+					setup_postdata( $post )
+				?>
+
+				<!-- Create variables for testimonials -->
+				<?php 
+					$t_first_name			= get_field('practitioner_first_name');
+					$t_last_name      = get_field('practitioner_last_name'); 								
+					$t_text       		= get_field('testimonial_text'); 
+				 	$t_client_name 		= get_field('client_name'); 
+				 	$t_photo 					= get_field('testimonial_image');
+
+				 	// Create array of testimonial information
+					if($t_last_name == $last_name):
+						$testimonials_array[] = array(
+						'first_name'		=> $t_first_name,
+						'last_name' 		=> $t_last_name, 
+						't_text'  			=> $t_text, 
+						't_client_name' => $t_client_name
+					);
+			  	endif; endforeach; wp_reset_postdata(); ?>
+
+			<div class="container">
+				<h1 style="text-align: center;">Individual Practitioner <?php the_title(); ?></h1>
+				<div class="row">
+					<div class="col-md-4">
+						<div>
+							<?php echo wp_get_attachment_image($p_photo, "thumbnail");?>
+						</div>
+						<div>
+							<h3><?php echo($first_name);?> <?php echo($last_name) ?></h3>  
+							<h4>Certifications: <?php echo($certifications);?></h4>
+							<h4>Practice: <?php echo($practice);?></h4>
+						</div>
+						<h3>Schedule an Appointment</h3>
+						<div>
+							<a href="mailto:<?php echo($email);?>"><?php echo($email);?></a>
+						</div>
+						<div>
+							<a href="tel:<?php echo($phone);?>"><?php echo($phone);?></a>
+						</div>
+						<h4>External Links</h4>
+						<div>
+							<?php echo($links); ?>
+						</div>
+					</div> <!-- end col-md-4 (sidebar) -->
+					<div class="col-md-8">
+						<div>
+							<?php echo($about); ?>
+						</div>
+						<div class="row">
+							<h1>Testimonials</h1>
+							<?php if(count($testimonials_array) >= 3): ?>
+							<div class="col-lg-4">
+								<div><?php echo $testimonials_array[0]['t_text'] ?></div>
+								<div><?php echo $testimonials_array[0]['t_client_name'] ?></div>
+							</div>
+							<div class="col-lg-4">
+								<div><?php echo $testimonials_array[1]['t_text'] ?></div>
+								<div><?php echo $testimonials_array[1]['t_client_name'] ?></div>
+							</div>
+							<div class="col-lg-4">
+								<div><?php echo $testimonials_array[2]['t_text'] ?></div>
+								<div><?php echo $testimonials_array[2]['t_client_name'] ?></div>
+							</div> 
+						<?php elseif(count($testimonials_array) == 2): ?>
+							<div class="col-lg-6">
+								<div><?php echo $testimonials_array[0]['t_text'] ?></div>
+								<div><?php echo $testimonials_array[0]['t_client_name'] ?></div>
+							</div>
+							<div class="col-lg-6">
+								<div><?php echo $testimonials_array[1]['t_text'] ?></div>
+								<div><?php echo $testimonials_array[1]['t_client_name'] ?></div>
+							</div>
+						<?php else: ?>
+							<div class="col-lg-6">
+								<div><?php echo $testimonials_array[0]['t_text'] ?></div>
+								<div><?php echo $testimonials_array[0]['t_client_name'] ?></div>
+							<?php endif; ?>
+						</div>
+					</div> <!-- end col-md-8 (body) -->
+				</div> <!-- end row -->
+			</div> <!-- end container -->
+
+
+
+			<?php endwhile; // End of the loop.?>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
+
