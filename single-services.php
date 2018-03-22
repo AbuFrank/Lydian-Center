9cc12cd83df51eb58421ae4ba9958a745796334d
juliesof <juliesof@gmail.com>
2018-03-22 16:33:38 -0400

    correct practitioners appear in sidebar for services page

--- single-services.php
+++ single-services.php
@@ -17,14 +17,14 @@ get_header();
 		while ( have_posts() ) : the_post();
 			// Add variables from ACF
 			$image =  get_field('service_image');
-
+			$service_type = get_field('service_type');
 			?>
 			<!-- Retrieve practioners object from database -->
 			<?php 
 					$practitioners = get_posts(array(
 						'post_type'				=> 'practitioners',
 						'posts_per_page'	=> -1,
-						'meta_key'				=> 'service_listing'
+						'meta_key'				=> 'service_name'
 					));
 				?>
 				<!-- Create accessible array of practitioners -->
@@ -43,7 +43,7 @@ get_header();
 					$practice       = get_field('practice'); 
 				 	$certifications = get_field('certifications'); 
 				 	$phone 					= get_field('phone_number');
-				 	$service 				= get_field('service_listing');
+				 	$p_service 				= get_field('service_name');
 				 	$email					= get_field('email');
 				 	$p_photo 				= get_field('practitioner_photo');
 
@@ -52,14 +52,14 @@ get_header();
 						'last_name' 					=> $last_name, 
 						'practice'  					=> $practice, 
 						'certifications'			=> $certifications,
-						'phone_number'  			=> $phone,
+						'phone'  							=> $phone,
 						'email'								=> $email,
-						'practitioner_photo' 	=> $p_photo,
-						'service_listing'			=> $service
+						'photo' 							=> $p_photo,
+						'p_service_name'			=> $p_service
 					);
 
 				  endforeach; wp_reset_postdata(); ?>
-		?>
+		
 
 			<div class="container">
 				<div class="row">
@@ -75,7 +75,30 @@ get_header();
 						<?php the_content();?>
 					</div> <!-- end main-content -->
 					<div class="col-md-4 sidebar-content">
-						<?php get_sidebar(); ?>
+						<?php $i = 0; ?>
+						<?php foreach($practitioners_array as $p): ?>
+						<?php if($p['p_service_name'] == $service_type):?>
+							<p>practitioner service: <?php echo $p['p_service_name'] ?></p>
+							<p>service service: <?php echo $service_type ?></p>
+							<?php echo wp_get_attachment_image($p['photo'], "thumbnail");?>
+							</div>
+							<div>
+								<h3><?php echo($p['first_name']);?> <?php echo($p['last_name']) ?></h3>  
+								<h4>Certifications: <?php echo($p['certifications']);?></h4>
+								<h4>Practice: <?php echo($p['practice']);?></h4>
+							</div>
+							<h3>Schedule an Appointment</h3>
+							<div>
+								<a href="mailto:<?php echo($p['email']);?>"><?php echo($email);?></a>
+							</div>
+							<div>
+								<a href="tel:<?php echo($p['phone']);?>"><?php echo($phone);?></a>
+							</div>
+							<h4>Learn more about</h4>
+							<div>
+								<p>link to other service page here</p>
+							</div>
+					<?php endif; endforeach; ?>
 					</div> <!-- end sidebar-content -->
 				</div>
 			</div>
